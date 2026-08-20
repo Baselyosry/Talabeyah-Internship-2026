d880f3c3b635ec7e38ade2b9d4b540dc5e3bcf0b
philosopher <200029096@must.edu.eg>
2026-08-20 13:52:59 +0300

    Add product service with validation for create and update

---
+++ Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+using Domain.Models;
+
+namespace Application.Interfaces;
+
+public interface IProductRepository
+{
+    Task<Product?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task AddAsync(Product product);
+    Task UpdateAsync(Product product);
+}

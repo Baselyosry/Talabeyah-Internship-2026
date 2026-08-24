54fe03bd2b987eb90bc2350357a961b44401594d
philosopher <200029096@must.edu.eg>
2026-08-24 12:45:42 +0300

    Add routing configuration for product list and login components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ProductList } from './features/products/pages/list/list';
+import { Login } from './features/auth/pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '', component: ProductList, children : [
+    {
+
+    }
+  ] },
+  { path: 'login', component: Login }
+];
